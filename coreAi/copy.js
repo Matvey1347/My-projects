97de8946be5f03068c574367eb8510ef7427806b
Pompey06 <dmytrouryvskyi@gmail.com>
2025-01-12 19:23:16 +0100

    WIP

---
+++ coreAi/copy.js
@@ -0,0 +1,49 @@
+const modal = document.querySelector(".copied_modal");
+
+const copies = document.querySelectorAll("[data-copy]");
+copies.forEach((copy) => {
+   // copy.addEventListener('click', () => {
+   const clipboard = new ClipboardJS(copy, {
+      text: function () {
+         // Получаем текст содержимого элемента
+         let text = copy.textContent.trim();
+
+         if (!text) {
+            text = copy.getAttribute("data-copy");
+         }
+
+         // Удаляем все <br> теги
+         text = text.replace(/<br\s*\/?>/gi, "");
+
+         return text;
+      },
+   });
+
+   clipboard.on("success", function (e) {
+      console.log("Текст успешно скопирован: " + e.text);
+      e.clearSelection();
+
+      // Добавляем класс _show к модальному окну
+      modal.classList.add("_show");
+
+      // Убираем класс _show через 2 секунды
+      setTimeout(() => {
+         modal.classList.remove("_show");
+      }, 2000);
+   });
+
+   clipboard.on("error", function (e) {
+      console.error("Ошибка при копировании");
+   });
+   // });
+});
+
+const button = document.querySelector(".button");
+
+let value = localStorage.getItem("test1");
+
+button.addEventListener("click", () => {
+   localStorage.setItem("test1", value + " как дела?");
+   value = localStorage.getItem("test1");
+   console.log(value);
+});
